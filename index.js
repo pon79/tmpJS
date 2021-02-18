7157f33e1754c2cbf2f4cd28b28d3c568b9433ff
pon79 <pon79job@gmail.com>
2021-02-18 15:00:42 +0300

    add index.json

---
+++ index.js
@@ -0,0 +1,14 @@
+
+alert("Hello!");
+
+
+// (async function() {
+//     let response = await fetch(url);
+
+//     if (response.ok) { // если HTTP-статус в диапазоне 200-299
+//     // получаем тело ответа (см. про этот метод ниже)
+//     let json = await response.json();
+//     } else {
+//     alert("Ошибка HTTP: " + response.status);
+//     }
+// })();
